14133aad6f59271b4431f10f785c477286344b49
Dulaj <balasuriyadulaj@gmail.com>
2020-08-05 18:34:57 +0530

    Upload

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,25 +7,31 @@ class App extends Component {
             <div>
                 <div className="jumbotron text-center">
                     <h1>LankanBuyer.COM</h1>
-                    <p>Resize this responsive page to see the effect!</p>
+                    <p>The best classified website so far. Coming soon!</p>
                 </div>
                 <div className="container">
                     <div className="row">
+                        <p>We bring you number of categories</p>
+                    </div>
+                    <div className="row">
                         <div className="col-sm-4">
-                            <h3>Column 1</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                            <h3>Buy/Sell Subscriptions</h3>
+                            <p>Sell your Spotify/HULU subscriptions.</p>
+                            <p>We offer you the standard rates</p>
                         </div>
                         <div className="col-sm-4">
-                            <h3>Column 2</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                            <h3>Web Application Development</h3>
+                            <p>If you want to sell your service for web development work. Try here!</p>
+                            <p>Not like Rs. 500 pages, we offer you standard rates</p>
                         </div>
                         <div className="col-sm-4">
-                            <h3>Column 3</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                            <h3>Social Marketing</h3>
+                            <p>Sell your marketing services, such as youtube watch times, facebook pages</p>
+                            <p>We bring you the best customers for you</p>
                         </div>
+                    </div>
+                    <div className="row">
+                        <p>The other best thing here is -- Registration is free!</p>
                     </div>
                 </div>
             </div>
