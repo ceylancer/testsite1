c748f31307451fb8a7626736ed5e1a8a380e4acb
Dulaj <balasuriyadulaj@gmail.com>
2020-08-05 12:16:12 +0530

    1

--- testsite1/settings.py
+++ testsite1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o*s^u1l=u(9a+(camf_7drtphizb5=66)i#0fam3d7!ob1n(*u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.lankanbuyer.com', '52.5.68.180']
+ALLOWED_HOSTS = ['lankanbuyer.com', '52.5.68.180', '127.0.0.1']
 
 
 # Application definition
